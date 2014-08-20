e70588dd70577a63bc3143a565216052fa172204
PengGongkui <mnlm1991@gmail.com>
2014-08-20 20:04:05 +0800

    1.对sort排序改用sort

--- sort_history/sort_history.sh
+++ sort_history/sort_history.sh
@@ -1,1 +1,1 @@
-history | awk '{a[$2]++;} END {for (i in a) printf "%5s %s\n", a[i],i;}' | awk '{a[$0]=$1} END {l=asorti(a,b); for (i=l; i > 0; --i) print b[i];}'
+history | awk '{a[$2]++;} END {for (i in a) printf "%5s %s\n", a[i],i;}' | sort -rn
